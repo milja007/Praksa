1bbe1ddfb066d77647749c49fe866764f25c1e48
Borna Miljković <borna.milja@gmail.com>
2025-06-01 20:00:15 +0200

    preparing all

--- components/form/broj/BrojFaktureDobavljaca.tsx
+++ components/form/broj/BrojFaktureDobavljaca.tsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const BrojFaktureDobavljaca = () => {
-  return <div>BrojFaktureDobavljaca</div>;
+  return (
+    <div>
+      <label
+        htmlFor="BrojFaktureDobavljaca"
+        className="text-sm font-medium text-gray-700"
+      >
+        Broj Fakture Dobavljaca:
+      </label>
+      <input
+        id="BrojFaktureDobavljaca"
+        type="text"
+        placeholder="broj fakture dobavljaca"
+        className="flex-grow appearance-none border border-gray-300 rounded-md px-3 py-1.5
+          text-sm text-gray-700 placeholder-gray-400
+          focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500
+          transition duration-150"
+      />
+    </div>
+  );
 };
 
 export default BrojFaktureDobavljaca;
